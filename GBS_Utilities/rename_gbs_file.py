9bbba004460a6e80e727b8780f82a31cfa4b2809
mlucas <mark.lucas.cork@gmail.com>
2018-08-09 16:29:59 +0100

    Initial Commit

--- GBS_Utilities/rename_gbs_file.py
+++ GBS_Utilities/rename_gbs_file.py
@@ -90,66 +90,99 @@ seqFilePath=args.path
 seqCenter=args.seqtype
 
 #------------------------------------------------------------------------
-
+gbsList=[]
 if seqCenter=='KSU':
     gbsProject=os.path.basename(os.path.normpath(seqFilePath)) # Get the KSU project name e.g. 1369_HGJ27BGX7
     gbsNumber='GBS'+ gbsProject.split('_')[0]
     gbsFlowcell=gbsProject.split('_')[1]
     gbsLane=0
-
-
-# SQL Statement to update the gbs record with flowcell and lane data.
-
-gbsFlowcellUpdate=("UPDATE gbs SET flowcell=%s WHERE gbs_id LIKE %s and flowcell is NULL" )
-gbsLaneUpdate=("UPDATE gbs SET lane=%s WHERE gbs_id LIKE %s and lane is NULL")
-
-# SQL Query to retrieve the gbs_name and dna_id from the gbs records for use in the file renaming.
-
-gbsQuery=("SELECT gbs_id,gbs_name,dna_id,flowcell,lane from gbs WHERE gbs_id LIKE %s")
-
-# Open database connection
-
-cursor, cnx = open_db_connection(local_config)
-try:
-    cursor.execute(gbsFlowcellUpdate,(gbsFlowcell,gbsNumber+'%'))
-    cursor.execute(gbsLaneUpdate, (gbsLane,gbsNumber + '%',))
-    cursor.execute(gbsQuery, (gbsNumber + '%',))
-    if cursor.rowcount != 0:
-        plateList=[]
-        for row in cursor:
-            gbsId=row[0][0:7]
-            gbsName=row[1]
-            gbsName= ''.join(e for e in gbsName if e.isalnum())
-            dnaPlates=plateList.append(row[2][9:])
-            flowCell=row[3]
-            lane=row[4]
-except Exception as e:
-    print('Unexpected error during database query:'+ str(e))
-    print('Exiting...')
-    sys.exit()
-    print('Closing connection to database table: gbs.')
-
-commit_and_close_db_connection(cursor, cnx)
-
-# Formulate GBS File Name
-
-dnaPlateString=''.join(plateList)
-
-gbsFileName=os.path.join(seqFilePath,'') + gbsId+'x'+gbsName+dnaPlateString+'_'+flowCell+'_'+'s'+'_'+ str(lane) + '_fastq.txt.gz'
-print(gbsFileName)
-
-if seqCenter=='KSU':
-    fileList=[]
+    gbsList.append([gbsNumber,gbsFlowcell,gbsLane])
+elif seqCenter=='Quebec':
+    gbsList = []
+    gbsFileList=[]
     for file in os.listdir(seqFilePath):
-        if (file.startswith(gbsProject.split('_')[0]) and file.endswith(".gz")):
-            fileList.append(os.path.join(seqFilePath, file))
-    fileList.sort()
+        if (file.startswith('HI.') and file.endswith(".gz")):
+            gbsPart=file.split('.')[3]
+            gbsNumber=gbsPart.split('_')[0]
+            gbsFile=os.path.join(seqFilePath,file)
+            with gzip.open(gbsFile,'rt') as handle:
+                firstRead = next(SeqIO.parse(handle, "fastq"))
+            gbsFlowcell=firstRead.id.split(':')[2]
+            gbsLane=int(firstRead.id.split(':')[3])
+            gbsList.append([gbsNumber,gbsFlowcell,gbsLane])
+            gbsFileList.append(gbsFile)
+    gbsList.sort()
+    gbsFileList.sort()
+print(gbsList)
+print(gbsFileList)
 
-    # Concatenate separate files into one GBS File
-    #with open((os.path.join(seqFilePath,'') + gbsFileName), 'wb') as outfile:
-    with open(gbsFileName, 'wb') as outfile:
-        for fname in fileList:
-            with open(fname,'rb') as infile:
-                shutil.copyfileobj(infile,outfile)
+index=0
+for gbs in gbsList:
+
+    gbsNumber=gbs[0]
+    gbsFlowcell=gbs[1]
+    gbsLane=gbs[2]
+
+    # SQL Statement to update the gbs record with flowcell and lane data.
+
+    gbsFlowcellUpdate=("UPDATE gbs SET flowcell=%s WHERE gbs_id LIKE %s and flowcell is NULL" )
+    gbsLaneUpdate=("UPDATE gbs SET lane=%s WHERE gbs_id LIKE %s and lane is NULL")
+
+    # SQL Query to retrieve the gbs_name and dna_id from the gbs records for use in the file renaming.
+
+    gbsQuery=("SELECT gbs_id,gbs_name,dna_id,flowcell,lane from gbs WHERE gbs_id LIKE %s")
+
+    # Open database connection
+
+    cursor, cnx = open_db_connection(local_config)
+    try:
+        cursor.execute(gbsFlowcellUpdate,(gbsFlowcell,gbsNumber+'%'))
+        cursor.execute(gbsLaneUpdate, (gbsLane,gbsNumber + '%',))
+        cursor.execute(gbsQuery, (gbsNumber + '%',))
+        if cursor.rowcount != 0:
+            plateList=[]
+            for row in cursor:
+                gbsId=row[0][0:7]
+                gbsName=row[1]
+                gbsName= ''.join(e for e in gbsName if e.isalnum())
+                dnaPlates=plateList.append(row[2][9:])
+                flowCell=row[3]
+                lane=row[4]
+    except Exception as e:
+        print('Unexpected error during database query:'+ str(e))
+        print('Exiting...')
+        sys.exit()
+        print('Closing connection to database table: gbs.')
+
+    commit_and_close_db_connection(cursor, cnx)
+
+    # Formulate GBS File Name
+
+    dnaPlateString=''.join(plateList)
+
+    gbsFileName=os.path.join(seqFilePath,'') + gbsId+'x'+gbsName+dnaPlateString+'_'+flowCell+'_'+'s'+'_'+ str(lane) + '_fastq.txt.gz'
+    print(gbsFileName)
+
+    if seqCenter=='KSU':
+        fileList=[]
+        for file in os.listdir(seqFilePath):
+            if (file.startswith(gbsProject.split('_')[0]) and file.endswith(".gz")):
+                fileList.append(os.path.join(seqFilePath, file))
+        fileList.sort()
+
+        # Concatenate separate files into one GBS File
+        #with open((os.path.join(seqFilePath,'') + gbsFileName), 'wb') as outfile:
+        with open(gbsFileName, 'wb') as outfile:
+            for fname in fileList:
+                with open(fname,'rb') as infile:
+                    shutil.copyfileobj(infile,outfile)
+    elif seqCenter=='Quebec':
+        with open(gbsFileName, 'wb') as outfile:
+            fname=gbsFileList[index]
+            with open(fname, 'rb') as infile:
+                shutil.copyfileobj(infile, outfile)
+    index+=1
+    print()
+
 sys.exit()
 
